792c7bf3c031d7cdb813fbdb0e33c62b691b0ef2
Alona <122108963+yelloally@users.noreply.github.com>
2023-03-04 00:53:14 +0100

    Add files via upload

---
+++ inheritance_of_interfaces/InterfacesOfAnimals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace inheritance_of_interfaces
+{
+	public interface InterfacesOfAnimals
+	{
+        //interface represents a method makeSound
+        interface IAnimal
+        {
+            void makeSound();
+        }
+
+        //interface represents a method giveBirth
+        interface IMammal : IAnimal
+        {
+            void giveBirth();
+        }
+    }
+}
+
---
+++ inheritance_of_interfaces/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace inheritance_of_interfaces
+{
+
+
+    //implements IMammal interface
+    class Dog : InterfacesOfAnimals.IMammal
+    {
+        private string breed;
+
+        public Dog(string breed)
+        {
+            this.breed = breed;
+        }
+
+        //implementation of the method
+        public void makeSound()
+        {
+            Console.WriteLine("The " + breed + " dog says woof!");
+        }
+
+        //implementation of the method
+        public void giveBirth()
+        {
+            Console.WriteLine("The " + breed + " dog gives birth to puppies!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What breed is your dog?");
+            string breed = Console.ReadLine();
+
+            InterfacesOfAnimals.IMammal myAnimal = new Dog(breed);
+
+            myAnimal.makeSound(); 
+            myAnimal.giveBirth(); 
+
+            Console.ReadKey();
+        }
+    }
+}
